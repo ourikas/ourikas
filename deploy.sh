08484c46da6e0ae0973b39ed50e50a645d24be49
Mauricio Melo <mauricio@mauriciomelo.com.br>
2015-07-11 13:46:59 -0300

    Add ourikas remote instead of update

--- deploy.sh
+++ deploy.sh
@@ -7,7 +7,7 @@ cp -R app tmp-deploy
 cd tmp-deploy
 git add .
 git commit --allow-empty-message -m ""
-git remote set-url ourikas git@github.com:Ourikas/ourikas.github.io.git
+git remote add ourikas git@github.com:Ourikas/ourikas.github.io.git
 git push ourikas :master
 git subtree push --prefix app ourikas master
 cd ..
