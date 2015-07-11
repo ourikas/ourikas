6fa80189e419a3e909bda1b8c97d3a002a0f7ef8
Mauricio Melo <mauricio@mauriciomelo.com.br>
2015-07-11 17:02:14 -0300

    Use push instead of push --subtree

--- deploy.sh
+++ deploy.sh
@@ -4,13 +4,13 @@ gulp manifest
 rm -Rf tmp-deploy
 mkdir tmp-deploy
 cp -R .git tmp-deploy
-cp -R app tmp-deploy
+cp -R app/* tmp-deploy
 cd tmp-deploy
 git add .
 git commit --allow-empty-message -m ""
 git remote add ourikas git@github.com:Ourikas/ourikas.github.io.git
 git push ourikas :master
-git subtree push --prefix app ourikas master
+git push ourikas master
 cd ..
 rm -Rf tmp-deploy
 echo "Done!"
