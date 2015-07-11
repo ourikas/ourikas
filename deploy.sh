4391172bf441f81a542784a955fcf68df6093d98
Mauricio Melo <mauricio@mauriciomelo.com.br>
2015-07-11 12:03:16 -0300

    Add deploy script

---
+++ deploy.sh
@@ -0,0 +1,13 @@
+npm install
+gulp build
+rm -Rf tmp-deploy
+mkdir tmp-deploy
+cp -R .git tmp-deploy
+cp -R app tmp-deploy
+cd tmp-deploy
+git commit -a --allow-empty-message -m ""
+git push origin :gh-pages
+git subtree push --prefix app origin gh-pages
+cd ..
+rm -Rf tmp-deploy
+echo "Done!"
