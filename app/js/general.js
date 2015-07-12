f036aff4f27f97eda97790439cf20ba8f99e3a14
Mauricio Melo <mauricio@mauriciomelo.com.br>
2015-07-12 19:49:09 -0300

    Open item when clicked

---
+++ app/js/general.js
@@ -0,0 +1,11 @@
+$('.search-result').on('click', '.company', function() {
+  $('.company-full').removeClass('company-full');
+  var $company = $(this);
+
+  $company.toggleClass('company-full');
+  setTimeout(function(){
+    $('html, body').animate({
+      scrollTop: $company.offset().top
+    }, 800);
+  }, 300);
+});
